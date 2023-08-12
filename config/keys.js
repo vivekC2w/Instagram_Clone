347980452e56e1d583f35594a314be3de34589f7
Vivek23699 <vivek23699@gmail.com>
2023-08-12 20:18:36 +0530

    Search functionality added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports={
-    MONGOURI:"mongodb+srv://hirevivek12:g7BBYn0fxVl8dyzj@cluster0.vounwoj.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET: "qazxswedcvfrtgbnhy"
+if(process.env.NODE_ENV==='production'){
+    module.exports = require('./prod')
+}else{
+    module.exports = require('./dev')
 }

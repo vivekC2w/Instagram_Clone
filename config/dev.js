d9a98bdd1dbf77e5196481e48d9fec2596ff7163
vivekC2w <68582416+vivekC2w@users.noreply.github.com>
2023-09-01 00:04:09 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://hirevivek12:<3cMhWQQU40zQwwZx>@cluster0.vounwoj.mongodb.net/?retryWrites=true&w=majority",
+    MONGOURI:"mongodb+srv://hirevivek12:3cMhWQQU40zQwwZx@cluster0.vounwoj.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET: "shjvsdjfvjdsvd",
     SENDGRID_API:"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",
     EMAIL:"http:localhost:3000"

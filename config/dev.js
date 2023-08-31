7a10bc3ab78a378b7ae65e7ef38ad3d704a1af00
vivekC2w <68582416+vivekC2w@users.noreply.github.com>
2023-08-31 14:07:07 +0530

    Updated password

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://hirevivek12:g7BBYn0fxVl8dyzj@cluster0.vounwoj.mongodb.net/?retryWrites=true&w=majority",
+    MONGOURI:"mongodb+srv://hirevivek12:3cMhWQQU40zQwwZx@cluster0.vounwoj.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET: "qazxswedcvfrtgbnhy",
     SENDGRID_API:"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",
     EMAIL:"http:localhost:3000"
